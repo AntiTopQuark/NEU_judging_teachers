595aab7f8bce92a0893532153df1d2a898d28626
DeleteZN <32798045+DeleteZN@users.noreply.github.com>
2020-12-17 21:12:17 +0800

    Update main.js
    学校开始使用新的系统

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-list = document.getElementsByClassName("option-list")
-for (var i = 0; i < list.length; i++) {
-list[i].children[0].children[1].click()
+// 历史版本
+// list = document.getElementsByClassName("option-list")
+// for (var i = 0; i < list.length; i++) {
+// list[i].children[0].children[1].click()
+// }
+// jQuery("#sub").click()
+
+// tqme.neu.edu.cn版本 2020年12月17日
+list = $("input[value='SPArvJbozwQQ7vMZVZm6dP']")
+for(var i = 0; i < list.length;i++){
+    list[i].click()
 }
-jQuery("#sub").click()
+document.getElementById("btn-saveResult").click()
